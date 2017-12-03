cb753b31ebc7082b6b03dce000c9c34a464ef670
Евгений <dev@aspres.ru>
2017-12-03 19:09:52 +0300

    Update Lab3.sql

--- Lab3.sql
+++ Lab3.sql
@@ -1,4 +1,4 @@
-/* a) Найти самые популярные издания
+/* a) Найти самые популярные издания  */
 SELECT RANK() OVER (ORDER BY SUM(bc.Count) DESC) AS Rank, Publishers.Name, SUM(bc.Count) as Rating
 FROM Books b
 JOIN (
@@ -8,17 +8,37 @@ JOIN (
 ) bc ON (b.Id = bc.BookId)
 JOIN Publishers ON (Publishers.Id = b.Publisher)
 GROUP BY Publishers.Id, Publishers.Name
- */
-/* б) Найти темы, по которым все экземпляры книг находятся на руках */
 
-/* в) Составить список книг по указанной теме 
+/* б) Найти темы, по которым все экземпляры книг находятся на руках  */
+SELECT t1.Name
+FROM (
+	SELECT t.Name, tb.ThemeId, COUNT(*) AS Count
+	FROM Themes t
+	JOIN Themes_Books tb ON (t.Id = tb.ThemeId)
+	GROUP BY tb.ThemeId, t.Name
+) t1
+JOIN (
+	SELECT tbb.ThemeId, COUNT(*) as Count
+	FROM Books b
+	JOIN (
+		SELECT c.BookId, COUNT(*) as Amount
+		FROM Readers_Books rb
+		JOIN Copies c ON (c.Id = rb.CopiesId)
+		WHERE rb.ReturnDate IS NULL
+		GROUP BY c.BookId
+	) bc ON (b.Id = bc.BookId AND b.Amount = bc.Amount)
+	JOIN Themes_Books tbb ON (b.Id = tbb.BookId)
+	GROUP BY tbb.ThemeId
+) t2 ON (t1.ThemeId = t2.ThemeId AND t1.Count = t2.Count)
+
+/* в) Составить список книг по указанной теме */
 SELECT book.Name
 FROM Themes_Books theme 
 JOIN Books book ON theme.BookId = book.Id 
 WHERE theme.ThemeId = 1
-*/
+
 /* г) Составить список должников, у которых срок долга > 1 месяца (ФИО, шифр, автор, название, дата сдачи)
-, отсортировать по убыванию срока долга (т.е, в начала - самые злостные должники) 
+, отсортировать по убыванию срока долга (т.е, в начала - самые злостные должники) */
 SELECT r.FIO, b.Code, a.FIO, b.Name, rb.ReturnDate
 FROM Readers_Books rb 
 JOIN Readers r ON rb.ReaderId = r.Id
@@ -31,5 +51,16 @@ WHERE DATEDIFF(
 	DATEADD(DAY, rb.TimeConstraint, rb.DateOfIssue), 
 	rb.ReturnDate
 ) > 1
-*/
+
 /* д) Для каждой учебной группы выдавать количество книг, взятых с начала текущего года, количество должников */
+SELECT t1.RGroup, COUNT(*) as Books, SUM(CASE WHEN t1.Debtor = 'TRUE' THEN 1 ELSE 0 END ) as Debtors
+FROM (
+	SELECT r.Id, r.RGroup,
+	CASE
+		WHEN (rb.TimeConstraint - (DATEDIFF(DAY, rb.DateOfIssue, GETDATE())) < 0 AND rb.ReturnDate IS NULL) 
+		THEN 'TRUE' ELSE 'FALSE' END Debtor
+	FROM Readers r 
+	RIGHT JOIN Readers_Books rb ON (r.Id = rb.ReaderId)
+	WHERE DATEDIFF(YEAR, rb.DateOfIssue, GETDATE()) = 0
+) t1
+GROUP BY t1.Id, t1.RGroup
